e034c01d6e31941bda86df6549d89ebac012dc84
Duy Nguyen <duynguyenngoc@hotmail.com>
2023-02-23 10:31:13 +0700

    E-66 :apple: API process Android Restore / Recovery mode

--- backend/app/api/resources/v1/restore/background/android.py
+++ backend/app/api/resources/v1/restore/background/android.py
@@ -3,11 +3,10 @@ import json
 from utils.file import save_file
 from utils.time import now_utc
 from init import redis
-import json
-import time
 from utils.android_report import make_report
 from utils.file import load_file
 from settings import config
+import subprocess
 
 
 def restore_process(request_id: str, device_id: str, report_name: str):
@@ -48,37 +47,42 @@ def restore_process(request_id: str, device_id: str, report_name: str):
     redis.set(request_id, json.dumps(data))
     
     try:
-        print('[{}] in while loop ...'.format(request_id))
-    
+        print('\n[{}] Start Restore ...'.format(request_id))
         data['progress']['name'] = 'Get Info of product'
         data['progress']['percent'] = 20
         data['logs'].append('Get Info of product -> Done')
         data['status']['get_product'] = 'success'
-        redis.set(request_id, json.dumps(data))
-        time.sleep(2)
         
         data['progress']['name'] = 'Get Info of battery'
         data['progress']['percent'] = 50
         data['logs'].append('Get Info of battery -> Done')
         data['status']['get_battery'] = 'success'
-        redis.set(request_id, json.dumps(data))
-        time.sleep(2)
         
         data['progress']['name'] = 'Get info of System'
         data['progress']['percent'] = 70
         data['logs'].append('Get Info of System -> Done')
         data['status']['get_system'] = 'success'
-        redis.set(request_id, json.dumps(data))
-        time.sleep(2)
         
         data['progress']['name'] = 'Make Report'
         data['progress']['percent'] = 90
         data['logs'].append('Make Report -> Done')
         data['status']['make_report'] = 'success'
         redis.set(request_id, json.dumps(data))
-        time.sleep(2)
+
+        # if info['ro.product.brand'] == 'samsung':
+        try:
+            s_info = subprocess.Popen(["adb", "-s", "%s" %(device_id), "reboot", "recovery"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            stdout = s_info.stdout
+            for line in stdout:
+                _str = line.decode('utf-8')
+                data['logs'].append(_str)
+            data['logs'].append("Report successful but we can't restore the device. Please do it manually on the device screen")
+        except Exception as e:
+            data['logs'].append(str(e))
+        finally:
+            redis.set(request_id, json.dumps(data))
         
-        data['new_os'] = "Android 11"
+        data['new_os'] = info['ro.build.version.release']
         data['progress']['status'] = True
         data['progress']['percent'] = 100
         data['progress']['step'] = 12
@@ -86,7 +90,6 @@ def restore_process(request_id: str, device_id: str, report_name: str):
         data['times']['end'] = now_utc().timestamp()
         data['status']['general'] = 'success'
         redis.set(request_id, json.dumps(data))
-        print('[{}] Done!'.format(request_id))
         
     except Exception as e:
         data['progress']['status'] = False
@@ -99,7 +102,7 @@ def restore_process(request_id: str, device_id: str, report_name: str):
         
     finally:
         save_file(config.STORAGE_DEFAULT_PATH + 'data_logs/{}.json'.format(request_id + '_' + device_id), data)
-        print('[{}] Save logs file success'.format(request_id))
+        print('[{}] Save logs file success!'.format(request_id))
         make_report(report_name, data, info) 
-        print('[{}] Make report {} success'.format(request_id, report_name+ '.pdf'))
+        print('[{}] Make report {} success!'.format(request_id, report_name+ '.pdf'))
         
